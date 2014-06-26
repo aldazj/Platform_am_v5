2830c4986d13e600380f832dcfd3d9fecc47bff5
aldazj <jayroaldaz@hotmail.com>
2014-06-26 11:28:06 +0200

    Platform_am_v5 Mails

---
+++ app/models/right.rb
@@ -0,0 +1,4 @@
+class Right < ActiveRecord::Base
+    has_and_belongs_to_many :groups, :join_table => 'groups_rights'
+    has_and_belongs_to_many :personalgroups, :join_table => 'personal_groupsrights'
+end
